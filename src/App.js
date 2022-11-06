0f85e2b888ab5216c897be3f77e7a8ceb6d5ca5a
Kacper Nalepa <kacper.nlp@wp.pl>
2022-11-06 12:33:45 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Header</h1>
+      <div className="content">Hello content</div>
+    </div>
+  );
+}
+
+export default App;
